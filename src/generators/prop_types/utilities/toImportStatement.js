b6603a88bef0b8860d57c1b6c84b6a24e8a46361
Christian Muñoz <christian.munoz@flow.io>
2018-05-23 12:04:37 -0400

    Fix incorrect path generated for import statements

--- src/generators/prop_types/utilities/toImportStatement.js
+++ src/generators/prop_types/utilities/toImportStatement.js
@@ -1,3 +1,4 @@
+const invariant = require('invariant');
 const path = require('path');
 const toModuleName = require('./toModuleName');
 const toDefaultExport = require('./toDefaultExport');
@@ -8,11 +9,22 @@ const toDefaultExport = require('./toDefaultExport');
  * @param {Entity} target - The entity to be imported.
  */
 function toImportStatement(source, target) {
+  invariant(source != null, 'A source entity must be provided');
+  invariant(target != null, 'A target entity must be provided');
   const sourcePath = toModuleName(source);
   const targetPath = toModuleName(target);
+  // console.log(path.resolve(sourcePath, targetPath));
+  let moduleName;
+
+  if (path.dirname(sourcePath) === path.dirname(targetPath)) {
+    moduleName = `./${path.basename(targetPath)}`;
+  } else {
+    moduleName = path.relative(path.dirname(sourcePath), targetPath);
+  }
+
   return {
     defaultExport: toDefaultExport(target),
-    moduleName: path.relative(sourcePath, targetPath),
+    moduleName,
   };
 }
 
--- test/specs/generators/prop_types/utilities/toImportStatement.spec.js
+++ test/specs/generators/prop_types/utilities/toImportStatement.spec.js
@@ -6,14 +6,36 @@ const toImportStatement = require('../../../../../src/generators/prop_types/util
 const service = new Service({ service: schema });
 
 describe('toImportStatement', () => {
-  test('should return import statement relative from one entity to another', () => {
+  test('should return import statement relative modules in the same "package"', () => {
     // com/bryzek/apidoc/api/v0/models/attributeValueFormPropTypes
     const source = find(service.models, { shortName: 'attribute_value_form' });
-    // com/bryzek/apidoc/common/v0/models/healthcheckPropTypes
-    const target = find(service.models, { shortName: 'healthcheck' });
+    // com/bryzek/apidoc/api/v0/models/attributeValuePropTypes
+    const target = find(service.models, { shortName: 'attribute_value' });
     expect(toImportStatement(source, target)).toEqual({
-      defaultExport: 'healthcheckPropTypes',
-      moduleName: '../../../../common/v0/models/healthcheckPropTypes',
+      defaultExport: 'attributeValuePropTypes',
+      moduleName: './attributeValuePropTypes',
+    });
+  });
+
+  test('should return import statement relative to modules in the same API builder "namespace"', () => {
+    // com/bryzek/apidoc/api/v0/models/originalPropTypes
+    const source = find(service.models, { shortName: 'original' });
+    // com/bryzek/apidoc/api/v0/enums/originalTypePropTypes
+    const target = find(service.enums, { shortName: 'original_type' });
+    expect(toImportStatement(source, target)).toEqual({
+      defaultExport: 'originalTypePropTypes',
+      moduleName: '../enums/originalTypePropTypes',
+    });
+  });
+
+  test('should return import statement relative to modules in a different API builder namespace', () => {
+    // com/bryzek/apidoc/api/v0/models/subscriptionPropTypes
+    const source = find(service.models, { fullyQualifiedName: 'com.bryzek.apidoc.api.v0.models.subscription' });
+    // com/bryzek/apidoc/common/v0/models/auditPropTypes
+    const target = find(service.models, { fullyQualifiedName: 'com.bryzek.apidoc.common.v0.models.audit' });
+    expect(toImportStatement(source, target)).toEqual({
+      defaultExport: 'auditPropTypes',
+      moduleName: '../../../common/v0/models/auditPropTypes',
     });
   });
 });
