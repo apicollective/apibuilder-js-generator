a7a471e2dd1dbad5f852a21db9963c965ca73e0b
Christian Muñoz <christian.munoz@flow.io>
2018-06-02 00:33:50 -0400

    Refactor entities in path to generate a GraphQLEnumType

--- src/generators/node_graphql/generators/enumeration/index.js
+++ src/generators/node_graphql/generators/enumeration/index.js
@@ -2,16 +2,18 @@ const path = require('path');
 
 const { renderTemplate } = require('../../../../utilities/template');
 const toDefaultExport = require('../../utilities/toDefaultExport');
-const GraphQLEnumTypeConfig = require('../../utilities/GraphQLEnumTypeConfig');
+const GraphQLEnumType = require('../../utilities/GraphQLEnumType');
 
 /**
+ * Generates code for a GraphQLEnumType from an ApiBuilderEnum.
  * @param {ApiBuilderEnum} enumeration
+ * @returns {String}
  */
 function generateCode(enumeration) {
-  const templatePath = path.resolve(__dirname, './templates/enumeration.ejs');
+  const templatePath = path.resolve(__dirname, './templates/GraphQLEnumType.ejs');
   return renderTemplate(templatePath, {
     exportName: toDefaultExport(enumeration),
-    enumeration: GraphQLEnumTypeConfig.fromEnum(enumeration),
+    enumeration: GraphQLEnumType.fromApiBuilderEnum(enumeration),
   });
 }
 
