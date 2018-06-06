b0d3ca399ab663ad2f4277c6589de5ab2173c3ba
Ben Iofel <ben@flow.io>
2018-06-06 13:58:11 -0400

    remove generated readme

--- src/generators/node_graphql/generators/schema/index.js
+++ src/generators/node_graphql/generators/schema/index.js
@@ -6,28 +6,12 @@ const { generateFile: generateEnumFile } = require('../enumeration');
 const { generateFile: generateModelFile } = require('../model');
 const { generateFile: generateUnionFile } = require('../union');
 
-function generateReadme(service) {
-  const templatePath = path.resolve(__dirname, './templates/readme.ejs');
-  const templateData = { service };
-  return renderTemplate(templatePath, templateData, { prettier: false });
-}
-
-function generateReadmeFile(service) {
-  const destinationPath = 'README.md';
-  const basename = path.basename(destinationPath);
-  const dirname = path.dirname(destinationPath);
-  const content = generateReadme(service);
-  return new ApiBuilderFile(basename, dirname, content);
-}
-
 /**
  * @param {ApiBuilderService} service
  * @returns {ApiBuilderFile[]}
  */
 function generateFiles(service) {
   const files = [];
-
-  files.push(generateReadmeFile(service));
 
   service.internalEnums.forEach((enumeration) => {
     files.push(generateEnumFile(enumeration));
