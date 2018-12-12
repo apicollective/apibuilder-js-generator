bdb3b7f85e1ce81c4c23e6474dc5e009bc070ce5
Christian Muñoz <christian.munoz@flow.io>
2018-12-12 12:23:37 -0500

    upgrade apibuilder reference library to latest version

--- src/generators/ts_constants/index.js
+++ src/generators/ts_constants/index.js
@@ -1,43 +1,22 @@
 const { ApiBuilderFile, ApiBuilderService } = require('apibuilder-js');
-const path = require('path');
+const { reduce, sortBy, uniqBy } = require('lodash');
+const { resolve } = require('path');
 const { renderTemplate } = require('../../utilities/template');
-
-function shortNameCompare(a, b) {
-  if (a.shortName > b.shortName) {
-    return 1;
-  }
-
-  if (a.shortName < b.shortName) {
-    return -1;
-  }
-
-  return 0;
-}
-
-function generateEnumFiles(service) {
-  const templatePath = path.resolve(__dirname, './templates/enumeration.ejs');
-  return service.enums.reduce((files, enumeration) => {
-    const basename = `${enumeration.nickname}.ts`;
-    const dirname = '.';
-    const contents = renderTemplate(templatePath, { enumeration }, { prettier: false });
-    const file = new ApiBuilderFile(basename, dirname, contents);
-    return files.concat(file);
-  }, []);
-}
-
-function generateIndexFile(service) {
-  const templatePath = path.resolve(__dirname, './templates/index.ejs');
-  const basename = 'index.ts';
-  const dirname = '.';
-  const contents = renderTemplate(templatePath, {
-    enumerations: service.enums.sort(shortNameCompare),
-  }, { prettier: false });
-  const file = new ApiBuilderFile(basename, dirname, contents);
-  return file;
-}
 
 exports.generate = function generate(invocationForm) {
   const service = new ApiBuilderService(invocationForm.service);
-  const files = generateEnumFiles(service).concat(generateIndexFile(service));
-  return Promise.resolve(files);
+  const templatePath = resolve(__dirname, './templates/enumerations.ejs');
+  const dirname = service.namespace.split('.').join('/');
+  const basename = `enums.ts`;
+  const contents = renderTemplate(templatePath, {
+    enumerations: sortBy(service.enums, 'nickname'),
+  }, {
+    prettier: {
+      parser: 'typescript',
+      singleQuote: true,
+      trailingComma: 'es5',
+    },
+  });
+  const file = new ApiBuilderFile(basename, dirname, contents);
+  return Promise.resolve([file]);
 };
--- src/utilities/template.js
+++ src/utilities/template.js
@@ -3,18 +3,18 @@ const fs = require('fs');
 const prettier = require('prettier');
 
 const defaultOptions = {
-  prettier: true,
+  prettier: {
+    singleQuote: true,
+    trailingComma: 'es5',
+    parser: 'babylon',
+  },
 };
 
 function renderTemplate(filepath, data, options = defaultOptions) {
   const template = fs.readFileSync(filepath, 'utf8');
   const compiled = ejs.compile(template, { filename: filepath });
   const code = compiled(data);
-  return !options.prettier ? code : prettier.format(code, {
-    singleQuote: true,
-    trailingComma: 'es5',
-    parser: 'babylon',
-  });
+  return options.prettier === false ? code : prettier.format(code, options.prettier);
 }
 
 exports.renderTemplate = renderTemplate;
