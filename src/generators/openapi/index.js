cd36b92b0bb86bcf672046a6e29a67876e15b10e
Nathan Stilwell <nathan@flow.io>
2019-02-01 14:30:46 -0500

    Fleshing out info section
    Note that this change is dependent on a change to
    apicollective/apibuilder-js

--- src/generators/openapi/index.js
+++ src/generators/openapi/index.js
@@ -1,12 +1,24 @@
 const createLogger = require('debug');
 const { ApiBuilderFile, ApiBuilderService } = require('apibuilder-js');
 
+const debug = createLogger('apibuilder:openapi');
+
 function generate(invocationForm) {
+    const service = new ApiBuilderService(invocationForm.service);
+    const info = {
+        title: service.name,
+        description: service.description,
+        termsOfService: '',
+        contact: service.info.contact,
+        license: service.info.license,
+        version: service.version,
+    };
     const contents = {
-        foo: 'bar',
+       openapi: "3.0.2",
+       info,
     };
 
-    const file = new ApiBuilderFile('foo', '', contents);
+    const file = new ApiBuilderFile('openapi.json', '', contents);
     return Promise.resolve([file]);
 }
 
