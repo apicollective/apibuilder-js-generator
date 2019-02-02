5c0ca47d09f7e5dee399b1579a3f1958d276a7d2
Nathan Stilwell <nathan@flow.io>
2019-02-01 19:04:08 -0500

    first steps toward converting Resources into a Paths Object

--- src/generators/openapi/index.js
+++ src/generators/openapi/index.js
@@ -1,7 +1,49 @@
 const createLogger = require('debug');
 const { ApiBuilderFile, ApiBuilderService } = require('apibuilder-js');
+const get = require('lodash/get');
+const map = require('lodash/map');
+const omit = require('lodash/omit');
+const pick = require('lodash/pick');
+const reduce = require('lodash/reduce');
+const groupBy = require('lodash/groupBy');
+const mapValues = require('lodash/mapValues');
+const flatten = require('lodash/flatten');
 
 const debug = createLogger('apibuilder:openapi');
+
+function extractOperation(resources) {
+    const mapped = map(resources, resource => resource.operations);
+    const flattened = flatten(mapped);
+    return flattened;
+}
+
+function resourcesToPaths(resources) {
+    const operations = extractOperation(resources);
+
+    // sort by path
+    const groupedOperations = groupBy(operations, (operation) => {
+      return operation.path;
+    });
+
+    // debug(`grouped: ${JSON.stringify(groupedOperations)}`);
+
+    // const filtered = mapValues(groupedOperations, (ops) => {
+    //   return map(ops, (op) => omit(op, ['path']));
+    // });
+
+    // debug(`filtered ${JSON.stringify(filtered)}`);
+
+    const paths = mapValues(groupedOperations, (operations) => {
+    //   debug(`operations ${JSON.stringify(operations)}`);
+      return reduce(operations, (props, operation, index) => {
+        // debug('operation', index, JSON.stringify(operation));
+        props[operation.method.toLowerCase()] = pick(operation, ['description']);
+        return props;
+      }, {});
+    });
+
+    return paths;
+}
 
 function generate(invocationForm) {
     const service = new ApiBuilderService(invocationForm.service);
@@ -9,13 +51,17 @@ function generate(invocationForm) {
         title: service.name,
         description: service.description,
         termsOfService: '',
-        contact: service.info.contact,
-        license: service.info.license,
+        contact: get(service, 'info.contact', ''),
+        license: get(service, 'info.license', ''),
         version: service.version,
     };
+
+    const paths = resourcesToPaths(service.resources);
+
     const contents = {
        openapi: "3.0.2",
        info,
+       paths,
     };
 
     const file = new ApiBuilderFile('openapi.json', '', contents);
