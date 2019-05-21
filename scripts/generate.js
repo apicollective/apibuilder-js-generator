a700a0776562de4748760d19c5dde1316a99cf7b
Christian Muñoz <christian.munoz@flow.io>
2019-05-21 13:04:51 -0400

    create proper invocation form in helper script

--- scripts/generate.js
+++ scripts/generate.js
@@ -2,6 +2,13 @@
 
 /* eslint-disable import/no-extraneous-dependencies */
 
+if (!process.env.APIBUILDER_TOKEN) {
+  throw new Error(
+    'The APIBUILDER_TOKEN environment variable must be set '
+    + 'to your APIBuilder token.'
+  );
+}
+
 const debug = require('debug');
 const program = require('commander');
 const fs = require('fs');
@@ -11,28 +18,47 @@ const path = require('path');
 
 const log = debug('apibuilder:generator');
 
-let serviceJson;
+let stdin = '';
 
-// ./scripts/generate.js ./api.json --generator jsdoc --output ./generated
+// USAGE:
+//
+// $ apibuilder download flow api latest | ./script/generate.js --generator jsdoc --output ./generated
+//
+// $ ./scripts/generate.js ${cat api.json} --generator jsdoc --output ./generated
 
-program
-  .version('0.1.0')
-  .arguments('<source>')
-  .option('--generator [generator]', 'name of generator to invoke', 'prop_types')
-  .option('--output [output]', 'path to directory where files will be generated', path.resolve(process.cwd(), './generated'))
-  .action((source) => {
-    serviceJson = path.resolve(process.cwd(), source);
-  })
-  .parse(process.argv);
+async function buildInvocationForm(schema) {
+  const uris = schema.imports.map(({ organization, application }) => (
+    `https://api.apibuilder.io/${organization.key}/${application.key}/latest`
+  ));
 
-serviceJson = JSON.parse(fs.readFileSync(serviceJson, 'utf8'));
+  log(`fetching imported services from: ${JSON.stringify(uris)}`);
 
-async function generate() {
-  const url = `http://localhost:7050/invocations/${program.generator}`;
-  const body = { service: serviceJson };
-  log(`Sending POST with invocation form to "${url}"`);
+  const promises = uris.map((uri) => got.get(uri, {
+    json: true,
+    headers: {
+      authorization: `Basic ${new Buffer(process.env.APIBUILDER_TOKEN).toString('base64')}`,
+    },
+  }));
+  const responses = await Promise.all(promises);
+  const importedServices = responses.map(response => response.body.service);
+  return {
+    attributes: [],
+    imported_services: importedServices,
+    service: schema,
+  };
+}
 
-  const response = await got.post(url, { json: true, body });
+async function generate(schema, options = {}) {
+  const { generator, output } = options;
+  const url = `http://localhost:7050/invocations/${generator}`;
+  const invocationForm = await buildInvocationForm(schema);
+
+  log(`sending POST with invocation form to "${url}"`);
+
+  const response = await got.post(url, {
+    json: true,
+    body: invocationForm,
+  });
 
   response.body.files.forEach((file) => {
     const destinationPath = path.resolve(program.output, file.dir, file.name);
@@ -42,5 +68,30 @@ async function generate() {
   });
 }
 
+program
+  .version('0.1.0')
+  .arguments('[schema]')
+  .option('--generator [generator]', 'name of generator to invoke', 'prop_types')
+  .option('--output [output]', 'path to directory where files will be generated', path.resolve(process.cwd(), './generated'))
+  .action((schema, options) => {
+    if (stdin) schema = stdin;
+    schema = JSON.parse(schema);
+    generate(schema, options).catch(console.error);
+  })
 
-generate().catch(console.error);
+
+if (process.stdin.isTTY) {
+  program.parse(process.argv);
+} else {
+  process.stdin.setEncoding('utf8');
+
+  process.stdin.on('readable', () => {
+    while ((chunk = process.stdin.read()) != null) {
+      stdin += chunk;
+    }
+  });
+
+  process.stdin.on('end', () => {
+    program.parse(process.argv);
+  });
+}
