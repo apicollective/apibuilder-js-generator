3b7a884fa71ed9dea748823fd1d689916c6675be
Christian Muñoz <christian.munoz@flow.io>
2019-06-12 00:38:55 -0400

    refactor helper script to accept invocation form

--- scripts/generate.js
+++ scripts/generate.js
@@ -1,13 +1,10 @@
 #!/usr/bin/env node
 
-/* eslint-disable import/no-extraneous-dependencies */
-
-if (!process.env.APIBUILDER_TOKEN) {
-  throw new Error(
-    'The APIBUILDER_TOKEN environment variable must be set '
-    + 'to your APIBuilder token.'
-  );
-}
+/**
+ * Usage:
+ * apibuilder code apicollective apibuilder-api latest form | ./scripts/generate.js
+ * apibuilder code apicollective apibuilder-api latest form | ./scripts/generate.js --generator ts_declarations
+ */
 
 const debug = require('debug');
 const program = require('commander');
@@ -15,44 +12,14 @@ const fs = require('fs');
 const got = require('got');
 const mkdirp = require('mkdirp');
 const path = require('path');
-const uniq = require('lodash/uniq');
 
 const log = debug('apibuilder:generator');
 
 let stdin = '';
 
-// USAGE:
-//
-// $ apibuilder download flow api latest | ./script/generate.js --generator jsdoc --output ./generated
-//
-// $ ./scripts/generate.js ${cat api.json} --generator jsdoc --output ./generated
-
-async function buildInvocationForm(schema) {
-  const uris = uniq(schema.imports.map(({ organization, application }) => (
-    `https://api.apibuilder.io/${organization.key}/${application.key}/latest`
-  )));
-
-  log(`fetching imported services from: ${JSON.stringify(uris)}`);
-
-  const promises = uris.map((uri) => got.get(uri, {
-    json: true,
-    headers: {
-      authorization: `Basic ${new Buffer(process.env.APIBUILDER_TOKEN).toString('base64')}`,
-    },
-  }));
-  const responses = await Promise.all(promises);
-  const importedServices = responses.map(response => response.body.service);
-  return {
-    attributes: [],
-    imported_services: importedServices,
-    service: schema,
-  };
-}
-
-async function generate(schema, options = {}) {
+async function postInvocationForm(invocationForm, options = {}) {
   const { generator, output } = options;
   const url = `http://localhost:7050/invocations/${generator}`;
-  const invocationForm = await buildInvocationForm(schema);
 
   log(`sending POST with invocation form to "${url}"`);
 
@@ -71,13 +38,13 @@ async function generate(schema, options = {}) {
 
 program
   .version('0.1.0')
-  .arguments('[schema]')
+  .arguments('[invocationForm]')
   .option('--generator [generator]', 'name of generator to invoke', 'prop_types')
   .option('--output [output]', 'path to directory where files will be generated', path.resolve(process.cwd(), './generated'))
-  .action((schema, options) => {
-    if (stdin) schema = stdin;
-    schema = JSON.parse(schema);
-    generate(schema, options).catch(console.error);
+  .action((invocationForm, options) => {
+    if (stdin) invocationForm = stdin;
+    invocationForm = JSON.parse(invocationForm);
+    postInvocationForm(invocationForm, options).catch(console.error);
   })
 
 
