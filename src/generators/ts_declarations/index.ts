cd1284ffe04074df64d1bc36cb94d93a7eaacc26
Christian Muñoz <christian.munoz@flow.io>
2019-08-01 14:00:25 -0400

    refactor generator with common builders

--- src/generators/ts_declarations/index.ts
+++ src/generators/ts_declarations/index.ts
@@ -1,30 +1,28 @@
-import { ApiBuilderFile, ApiBuilderService, ApiBuilderServiceConfig } from 'apibuilder-js';
+import { ApiBuilderFile } from 'apibuilder-js';
 import { print } from 'recast';
-import { buildFile } from './builders';
+import debug from 'debug';
 
-// tslint:disable-next-line:interface-name
-export interface InvocationForm {
-  readonly service: ApiBuilderServiceConfig;
-  readonly attributes: { [key: string]: string };
-  readonly user_agent?: string;
-  readonly imported_services?: ApiBuilderServiceConfig[];
-}
+import { buildFile } from './builders';
+import { InvocationForm, buildContext } from '../../builders';
+
+const log = debug('apibuilder:ts_declarations');
 
 export function generate(form: InvocationForm): Promise<ApiBuilderFile[]> {
   return new Promise((resolve) => {
-    const service = new ApiBuilderService(form.service);
-    const importedServices = form.imported_services != null
-      ? form.imported_services.map(importedService => new ApiBuilderService(importedService))
-      : [];
-    const ast = buildFile(service, importedServices);
-    const basename = `${service.applicationKey}.ts`;
-    const dirname = service.namespace.split('.').join('/');
+    log('INFO: Building context...');
+    const context = buildContext(form);
+    log('INFO: Building AST...');
+    const ast = buildFile(context);
+    log('INFO: Transforming AST to code...');
+    const basename = `${context.rootService.applicationKey}.ts`;
+    const dirname = context.rootService.namespace.split('.').join('/');
     const code = print(ast, {
       quote: 'single',
       tabWidth: 2,
       trailingComma: true,
       useTabs: false,
     }).code;
+    log('INFO: Code generation completed!');
     resolve([
       new ApiBuilderFile(basename, dirname, code),
     ]);
