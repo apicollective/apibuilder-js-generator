da8abaa0cae869cf53bd9bb2fff83ee1562af215
Ben Iofel <ben@flow.io>
2022-08-22 13:35:22 -0400

    openapi: discriminators (#89)

--- src/generators/openapi/generators/openapi-schemas/generateSchemaFromUnion.ts
+++ src/generators/openapi/generators/openapi-schemas/generateSchemaFromUnion.ts
@@ -1,6 +1,6 @@
 import { SchemaObject } from '@loopback/openapi-v3-types';
 import { ApiBuilderUnion } from 'apibuilder-js';
-import { map } from 'lodash';
+import { fromPairs, map } from 'lodash';
 import { convertApiBuilderType } from '../openapi-utils';
 import { IsImportedChecker } from '../openapi-utils/isTypeImported';
 
@@ -9,12 +9,26 @@ function generateSchemaFromUnion(
   typeValidation,
   isImported: IsImportedChecker,
 ): SchemaObject {
-  const types = map(union.types, t => t.type);
-  const unionTypes = map(types, t => convertApiBuilderType(t, typeValidation, isImported));
+  const data: [SchemaObject, string][] = union.types.map((t) => {
+    const type = convertApiBuilderType(t.type, typeValidation, isImported);
+    return [type, t.discriminatorValue];
+  });
+
+  const unionTypes = data.map(x => x[0]);
+  const mappings = fromPairs(
+    data.map(([type, discriminatorValue]) => [discriminatorValue, type.$ref]),
+  );
+
   return {
     [union.name]: {
       ...union.description && { description:  union.description },
       oneOf: unionTypes,
+      ...union.discriminator && {
+        discriminator: {
+          mapping: mappings,
+          propertyName: union.discriminator,
+        },
+      },
     },
   };
 }
