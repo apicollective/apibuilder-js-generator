b4e8c7c2b903d232d88f1fb8ee04e8a65803bc50
Rory Haddon <rory@flow.io>
2023-03-13 13:30:14 +0000

    TLAC-428: Remove disused generate code for 'node_graphql' (#106)

--- src/generators/node_graphql/utilities/typeMatches.ts
+++
@@ -1,9 +0,0 @@
-import { ApiBuilderBaseType, ApiBuilderType } from '../../../utilities/apibuilder';
-
-/**
- * Returns whether the type matches str, str_v2, str_v*...
- */
-export default function typeMatches(type: ApiBuilderBaseType, str: ApiBuilderType) {
-  const versioned = new RegExp(`^${str.toString()}(?:_v\\d+)?$`);
-  return type.fullyQualifiedType.fullyQualifiedType.match(versioned);
-}
